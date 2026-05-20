ad8f593810f7f0a0bc2528f12e9f1273061ac235
Nathaniel Abrante <nathaniel.lacerda@gmail.com>
2026-05-19 21:37:41 -0300

    feat: Add HealthController with a ping endpoint

--- src/main/java/com/rhacarys/contaconjunta/infra/config/SecurityConfig.java
+++ src/main/java/com/rhacarys/contaconjunta/infra/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/v1/ping").permitAll()
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated())
@@ -48,8 +49,7 @@ public class SecurityConfig {
         config.setAllowedOriginPatterns(List.of(
                 "http://localhost:*",
                 "https://racharcontas.vercel.app",
-                "https://racharcontas*rhacarys.vercel.app"
-        ));
+                "https://racharcontas*rhacarys.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
